2824cdcadf02dafb4cb2ddcbed4c78abfc608390
Ninpou <myse1lff@gmail.com>
2020-05-27 22:06:19 +0300

    Added header of home screen, added transparent status bar, fix drawer issue

--- Musio/app/src/main/java/com/example/musio/view/activity/MainActivity.java
+++ Musio/app/src/main/java/com/example/musio/view/activity/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -38,10 +40,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Make fullscreen
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         //INIT
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -85,6 +88,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     .commit();
 
         }
+
 
     }
 
