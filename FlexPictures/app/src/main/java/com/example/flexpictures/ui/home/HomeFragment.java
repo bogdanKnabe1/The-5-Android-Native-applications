144b495f2288481cb9ef4461097b520d34a9507b
Ninpou <myse1lff@gmail.com>
2020-06-29 17:37:50 +0300

    Clear other screens, add quality images.

--- FlexPictures/app/src/main/java/com/example/flexpictures/ui/home/HomeFragment.java
+++ FlexPictures/app/src/main/java/com/example/flexpictures/ui/home/HomeFragment.java
@@ -1,12 +1,15 @@
 package com.example.flexpictures.ui.home;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.flexpictures.R;
 
@@ -15,15 +18,25 @@ import com.example.flexpictures.R;
  */
 public class HomeFragment extends Fragment {
 
+    private HomeViewModel homeViewModel;
+
     public HomeFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(requireActivity(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
     }
 }
--- FlexPictures/app/src/main/java/com/example/flexpictures/ui/notifications/NotificationsFragment.java
+++ FlexPictures/app/src/main/java/com/example/flexpictures/ui/notifications/NotificationsFragment.java
@@ -2,11 +2,17 @@ package com.example.flexpictures.ui.notifications;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.flexpictures.R;
 
@@ -15,15 +21,19 @@ import com.example.flexpictures.R;
  */
 public class NotificationsFragment extends Fragment {
 
-    public NotificationsFragment() {
-        // Required empty public constructor
-    }
+    private NotificationsViewModel notificationsViewModel;
 
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        notificationsViewModel = new ViewModelProvider(this).get(NotificationsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        final TextView textView = root.findViewById(R.id.text_notifications);
+        notificationsViewModel.getText().observe(requireActivity(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
     }
 }
