8e2b25abc98ff5f99875f4fa3061b61431b4204d
Ninpou <myse1lff@gmail.com>
2020-06-29 16:40:41 +0300

    Added ItemModel(example), added card stack adapter, change styles.

---
+++ FlexPictures/app/src/main/java/com/example/flexpictures/adapter/CardStackAdapter.java
@@ -0,0 +1,74 @@
+package com.example.flexpictures.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.flexpictures.R;
+import com.example.flexpictures.model.ItemModel;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class CardStackAdapter extends RecyclerView.Adapter<CardStackAdapter.ViewHolder> {
+
+    private List<ItemModel> items;
+
+    public CardStackAdapter(List<ItemModel> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.item_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.setData(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+        TextView name, age, city;
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.item_image);
+            name = itemView.findViewById(R.id.item_name);
+            age = itemView.findViewById(R.id.item_age);
+            city = itemView.findViewById(R.id.item_city);
+        }
+
+        void setData(ItemModel data) {
+            Picasso.get()
+                    .load(data.getImage())
+                    .fit()
+                    .centerCrop()
+                    .into(image);
+            name.setText(data.getName());
+            age.setText(data.getAge());
+            city.setText(data.getCity());
+        }
+    }
+
+    public List<ItemModel> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemModel> items) {
+        this.items = items;
+    }
+}
---
+++ FlexPictures/app/src/main/java/com/example/flexpictures/model/ItemModel.java
@@ -0,0 +1,32 @@
+package com.example.flexpictures.model;
+
+public class ItemModel {
+    private int image;
+    private String name, age, city;
+
+    public ItemModel() {
+    }
+
+    public ItemModel(int image, String name, String age, String city) {
+        this.image = image;
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
