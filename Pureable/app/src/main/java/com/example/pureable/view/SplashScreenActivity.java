6d3f4459ef424ee736243102624553b39a9be2d0
Ninpou <myse1lff@gmail.com>
2020-07-11 18:30:27 +0300

    Added registration, added simple rules, added states to register and un register.

--- Pureable/app/src/main/java/com/example/pureable/view/SplashScreenActivity.java
+++ Pureable/app/src/main/java/com/example/pureable/view/SplashScreenActivity.java
@@ -1,21 +1,27 @@
 package com.example.pureable.view;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.Toast;
-
 import com.example.pureable.R;
+import com.example.pureable.model.DriverInfoModel;
 import com.example.pureable.utility.Common;
 import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -32,7 +38,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.Completable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Action;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -114,6 +119,56 @@ public class SplashScreenActivity extends AppCompatActivity {
     }
 
     private void showRegisterLayout() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.DialogTheme);
+        View itemView = LayoutInflater.from(this).inflate(R.layout.layout_register, null);
+
+        //find our input fields
+        TextInputEditText edt_first_name = itemView.findViewById(R.id.edt_first_name);
+        TextInputEditText edt_last_name = itemView.findViewById(R.id.edt_last_name);
+        TextInputEditText edt_phone = itemView.findViewById(R.id.edt_phone_number);
+
+        Button btnContinue = itemView.findViewById(R.id.btn_register);
+
+        //set data
+        if (FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber() != null
+            && !TextUtils.isEmpty(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber())){
+            edt_phone.setText(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber());
+
+            //set view
+            builder.setView(itemView);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+            btnContinue.setOnClickListener(v -> {
+                if (TextUtils.isEmpty(edt_first_name.getText().toString())){
+                    Toast.makeText(this, "Please enter first name", Toast.LENGTH_SHORT).show();
+                    return;
+                }else if (TextUtils.isEmpty(edt_last_name.getText().toString())){
+                    Toast.makeText(this, "Please enter last name", Toast.LENGTH_SHORT).show();
+                    return;
+                }else if (TextUtils.isEmpty(edt_phone.getText().toString())){
+                    Toast.makeText(this, "Please enter phone name", Toast.LENGTH_SHORT).show();
+                    return;
+                }else {
+                    DriverInfoModel model = new DriverInfoModel();
+                    model.setFirstName(edt_first_name.getText().toString());
+                    model.setLastName(edt_last_name.getText().toString());
+                    model.setPhoneNumber(edt_phone.getText().toString());
+                    model.setRating(0.0);
+
+                    driverInfoRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(model)
+                            .addOnFailureListener(e -> {
+                                dialog.dismiss();
+                                Toast.makeText(this, e.getMessage(),Toast.LENGTH_SHORT).show();
+                            })
+                            .addOnSuccessListener(aVoid -> {
+                                Toast.makeText(this, "Register successful!",Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                            });
+                }
+            });
+        }
 
     }
 
