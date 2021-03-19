a03376ad7c07153e2a575248ddbb389294d13b79
Ninpou <myse1lff@gmail.com>
2021-03-19 14:49:08 +0200

    Kotlin project start.

---
+++ Musio/app/src/main/java/com/example/musio/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.musio.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.musio.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
