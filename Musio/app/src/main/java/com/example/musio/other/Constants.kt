c2d8db6c391c031bc9217087386c2c6b7053eee9
Ninpou <myse1lff@gmail.com>
2021-03-19 21:39:41 +0200

    Implement firebase to project.

---
+++ Musio/app/src/main/java/com/example/musio/other/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.musio.other
+
+object Constants {
+
+    const val SONG_COLLECTION = "songs"
+}
--- Musio/app/src/main/java/com/example/musio/view/MainActivity.kt
+++ Musio/app/src/main/java/com/example/musio/view/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.musio.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.musio.R
+import com.example.musio.data.remote.MusicDatabase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var database: MusicDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        database = MusicDatabase()
+        GlobalScope.launch {
+            database.getAllSongs()
+            Log.d("TAG", "WE GET IT")
+        }
     }
 }
