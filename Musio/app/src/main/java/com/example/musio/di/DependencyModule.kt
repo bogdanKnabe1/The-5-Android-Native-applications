75ebd2c1b3bfc15e1ab53758c27088af4b4c5680
Ninpou <myse1lff@gmail.com>
2021-03-20 20:10:41 +0200

    Added stub for DI

---
+++ Musio/app/src/main/java/com/example/musio/di/DependencyModule.kt
@@ -0,0 +1,29 @@
+package com.example.musio.di
+
+import android.content.Context
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+
+// check DI
+object DependencyModule {
+
+    fun provideAudioAttributes() = AudioAttributes.Builder()
+            .setContentType(C.CONTENT_TYPE_MUSIC)
+            .setUsage(C.USAGE_MEDIA)
+            .build()
+
+    fun provideExoPlayer(
+            context: Context,
+            audioAttributes: AudioAttributes
+    ) = SimpleExoPlayer.Builder(context).build().apply {
+        setAudioAttributes(audioAttributes, true)
+        setHandleAudioBecomingNoisy(true)
+    }
+
+    fun provideDataSourceFactory(context: Context) =
+            DefaultDataSourceFactory(context, Util.getUserAgent(context, "Musio App"))
+
+}
--- Musio/app/src/main/java/com/example/musio/view/MainActivity.kt
+++ Musio/app/src/main/java/com/example/musio/view/MainActivity.kt
@@ -5,11 +5,15 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.musio.R
 import com.example.musio.data.remote.MusicDatabase
+import com.example.musio.di.DependencyModule
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var database: MusicDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
