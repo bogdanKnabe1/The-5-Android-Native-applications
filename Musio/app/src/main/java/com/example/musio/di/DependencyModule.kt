39b549829609bbde8fe280b1b7260a1abddf99e7
Ninpou <myse1lff@gmail.com>
2021-03-25 15:44:47 +0200

    Implement FirebaseMusicSource.kt.

--- Musio/app/src/main/java/com/example/musio/di/DependencyModule.kt
+++ Musio/app/src/main/java/com/example/musio/di/DependencyModule.kt
@@ -1,6 +1,7 @@
 package com.example.musio.di
 
 import android.content.Context
+import com.example.musio.data.remote.MusicDatabase
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.audio.AudioAttributes
@@ -8,8 +9,10 @@ import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 
 // check DI ( now service locator pattern )
+// Now An alternative to dependency injection is using a 'service locator'
 object DependencyModule {
 
+    fun provideMusicDatabase() = MusicDatabase()
 
     fun provideAudioAttributes() = AudioAttributes.Builder()
             .setContentType(C.CONTENT_TYPE_MUSIC)
